b3d14e2d5a63c66e097e732c541984c56afa656b
jaishii27 <royojasmin27@gmmail.com>
2026-01-27 10:14:09 +0800

    Added may project

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+<v-app>
+    <slot />
+</v-app>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-btn> Submit </v-btn>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>

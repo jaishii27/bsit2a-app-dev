4ddd5d86d73a16a7178205d83ce7d5d37b202fd0
jaishii27 <royojasmin27@gmail.com>
2026-02-03 10:24:26 +0800

    added layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,48 @@
 <template>
-<v-app>
-    <slot />
+<v-app> 
+    <v-navigation-drawer 
+        v-model="drawer"
+        permanent
+        :rail="rail"
+        
+      >
+        <template v-slot:prepend>
+          <v-list-item
+            lines="two"
+            prepend-avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDI6q1Z4xEdxhNDRQnmepQb-VRSJMwAmkSWg&s"
+            subtitle="Logged in"
+            title="Jai"
+          ></v-list-item>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click.stop="rail = !rail"></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+    <v-main>
+        <v-container fluid>
+            <slot />
+        </v-container>
+    </v-main>
 </v-app>
 
 </template>
 
-<script lang="ts" setup>
+<script  setup>
+const drawer = ref(true)
+const rail = ref(true)
 
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <v-btn> Submit </v-btn>
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="pink"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="blue"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="purple"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="yellow"></v-card>
+      </v-col>
+
+    </v-row>
     
   </div>
 </template>
